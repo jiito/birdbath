7b40fe2aff6b8d840f1d3f53d74ea337bb578136
benjamin-allanrahill <ballanrahill@middlebury.edu>
2022-02-24 20:13:46 -0500

    testing filter for likes!

--- pages/api/user/[username]/timeline.ts
+++ pages/api/user/[username]/timeline.ts
@@ -1,15 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import TwitterClient from "services/twitter";
-import { TweetUserTimelineV2Paginator, UserV2Result } from "twitter-api-v2";
+import { TweetV2 } from "twitter-api-v2";
 import { apiHandlerWithTwitter } from "utils/api/apiHandler";
+import { FilterFacotry } from "utils/FilterFactory";
 
 export default apiHandlerWithTwitter(async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<TweetUserTimelineV2Paginator>
+  res: NextApiResponse<TweetV2[]>
 ) {
+  const likes = parseInt(req.query.likes as string) || Infinity;
   const username = req.query.username as string;
   const id = await (await TwitterClient.getUserByName(username)).data.id;
   const userRes = await TwitterClient.getTweetsForUserById(id);
-  res.status(200).json(userRes);
+  const filteredTweets = await TwitterClient.filterTweets(
+    FilterFacotry.likesFilter(likes),
+    userRes.tweets
+  );
+  res.status(200).json(filteredTweets);
 });
