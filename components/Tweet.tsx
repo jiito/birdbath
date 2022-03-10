8ac6be214ad198e05ea7e12fbb06d7032b4378d3
benjamin-allanrahill <ballanrahill@middlebury.edu>
2022-03-09 23:21:01 -0500

    load tweets from filter

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -2,7 +2,9 @@ import { TweetV2 } from "twitter-api-v2";
 
 const Tweet = (tweet: TweetV2) => {
   return (
-    <p className="p-4 whitespace-pre-wrap rounded bg-slate-400">{tweet.text}</p>
+    <p className="p-4 whitespace-pre-wrap border border-gray-200 rounded">
+      {tweet.text}
+    </p>
   );
 };
 
--- pages/bath/index.tsx
+++ pages/bath/index.tsx
@@ -1,27 +1,44 @@
 import Layout from "components/Layout";
+import Tweet from "components/Tweet";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { TweetV2 } from "twitter-api-v2";
 const BathPage = () => {
-  const [loadedTweets, setLoadedTweets] = useState();
-  const onFormSubmit = (values: any) => {
-    fetch(
-      `api/user/timeline?filter=${values.filter}:${values.threshold}&total=${values.total}`
+  const session = useSession();
+  const [userId, setUserId] = useState<string>();
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      console.log(session);
+      setUserId((session.data as any).userId!);
+    }
+  }, [session]);
+  const [loadedTweets, setLoadedTweets] = useState<TweetV2[]>();
+  const onFormSubmit = async (values: any) => {
+    const res = await fetch(
+      `api/user/${userId}/timeline?filter=${values.filter}:${values.threshold}&total=${values.total}`
     );
+    const data = await res.json();
+    setLoadedTweets(data.tweets);
+  };
+
+  const deleteTweets = () => {
+    // delelet all the tweets from a filter
   };
   return (
     <Layout>
-      <div className=" max-w-2xl mx-auto my-20">
+      <div className="max-w-2xl mx-auto my-20 ">
         <Formik
           initialValues={{ total: "EVERY", filter: "like", threshold: 0 }}
-          onSubmit={(values) => console.log(values)}
+          onSubmit={(values) => onFormSubmit(values)}
         >
-          <div className="bg-gradient-to-bl from-green-300 via-blue-500 to-purple-600 p-1 rounded-md">
-            <Form className="form-container flex flex-col bg-white items-start py-8 px-12 rounded space-y-4">
-              <div className="flex text-lg font-semibold space-x-4">
+          <div className="p-1 rounded-md bg-gradient-to-bl from-green-300 via-blue-500 to-purple-600">
+            <Form className="flex flex-col items-start px-12 py-8 space-y-4 bg-white rounded form-container">
+              <div className="flex space-x-4 text-lg font-semibold">
                 <div>Delete</div>
                 <Field
                   name="total"
-                  className="w-24 appearance-none border border-gray-200 px-3 rounded flex items-center"
+                  className="flex items-center w-24 px-3 border border-gray-200 rounded appearance-none"
                 />
                 <div>tweet w/</div>
               </div>
@@ -36,20 +53,32 @@ const BathPage = () => {
                 </select>
                 <div>less than</div>
                 <Field
-                  className=" appearance-none border border-gray-200 px-3 rounded"
+                  className="px-3 border border-gray-200 rounded appearance-none "
                   name="threshold"
                   type="number"
                 />
               </div>
-              <button
-                className=" text-gray-50 py-1.5 px-3 rounded bg-gradient-to-bl from-green-300 via-blue-500 to-purple-600"
-                type="submit"
-              >
-                let's see the damage
-              </button>
+              {/* TODO: Abstract this into a fun button with different text for submission */}
+              <div className="flex flex-row">
+                <button
+                  className=" text-gray-50 py-1.5 px-3 mr-4 rounded bg-gradient-to-bl from-green-300 via-blue-500 to-purple-600"
+                  type="submit"
+                >
+                  let's see the damage
+                </button>
+                <button
+                  onClick={() => void 0}
+                  className=" cursor-pointer rounded text-red-600 py-1.5 px-3 border-red-600 border"
+                >
+                  don't look back
+                </button>
+              </div>
             </Form>
           </div>
         </Formik>
+        <div className="mt-4 space-y-4">
+          {loadedTweets && loadedTweets.map((t, i) => <Tweet {...t} key={i} />)}
+        </div>
       </div>
     </Layout>
   );
