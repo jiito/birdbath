2e452e997ca5be0d70f8e7347fa7fcfa612a4366
benjamin-allanrahill <ballanrahill@middlebury.edu>
2022-03-16 20:58:49 -0400

    throwing the error

--- utils/api/apiHandler.ts
+++ utils/api/apiHandler.ts
@@ -1,14 +1,11 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-import { getToken } from "next-auth/jwt";
 import { getSession } from "next-auth/react";
 import TwitterClient from "services/twitter";
 import { UserRepository } from "storage/UserRepository";
-import dbConnect from "utils/mongoose";
 
 export const apiHandlerWithTwitter = (handler: NextApiHandler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getSession({ req });
-    console.log(session);
 
     const { token, secret } = await UserRepository.getTwitterTokens(
       session!.userId as string
@@ -18,8 +15,8 @@ export const apiHandlerWithTwitter = (handler: NextApiHandler) => {
 
     try {
       await handler(req, res);
-    } catch (err) {
-      console.error(err);
+    } catch (err: any) {
+      throw new Error(err.message);
     }
   };
 };
