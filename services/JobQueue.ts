1122fedfb463abd356864e0d02ea2d01abe817bc
benjamin-allanrahill <benjamin.allanrahill@gmail.com>
2022-03-30 10:56:44 -0400

    refactor: mocing to a jobqueue mapping

---
+++ services/JobQueue.ts
@@ -0,0 +1,24 @@
+import { Job, Queue } from "bullmq";
+
+export enum QUEUES {
+  DELETE_TWEETS = "twitter_deleteTweets",
+}
+
+export class JobQueue {
+  q: Queue;
+  constructor(name: string, limiterKey?: string) {
+    this.q = new Queue(name, {
+      limiter: { groupKey: limiterKey! },
+    });
+  }
+
+  async addJob(title: string, data: any) {
+    await this.q.add(title, data);
+  }
+}
+
+export class JobQueues {
+  private static mapping: Map<QUEUES, JobQueue> = new Map([
+    [QUEUES.DELETE_TWEETS, new JobQueue(QUEUES.DELETE_TWEETS, "userId")],
+  ]);
+}
