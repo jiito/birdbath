4e098c77868c419ca047e59c1614e7e0b4a42516
benjamin-allanrahill <benjamin.allanrahill@gmail.com>
2022-06-19 08:41:55 -0400

    Adding emailTo param

--- services/SendgridAdapter.ts
+++ services/SendgridAdapter.ts
@@ -4,11 +4,11 @@ export class SendGridAdapter {
   private static initialize() {
     SendGridAdapter.service.setApiKey(process.env.SENDGRID_API_KEY!);
   }
-  static async sendEmail(subject: string, message: string) {
+  static async sendEmail(emailTo: string, subject: string, message: string) {
     SendGridAdapter.initialize();
     const email = SendGridAdapter.service.send({
       from: "baldwin@birdbath.me",
-      to: "benjamin.allanrahill@gmail.com",
+      to: emailTo,
       subject,
       text: message,
       html: "<p>This is a test email</p>",
